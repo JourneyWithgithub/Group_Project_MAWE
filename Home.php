c0bc2c41f55a198162cbd87217e63a45b02c2f2c
JourneyWithgithub <ajtamjid@gmail.com>
2023-10-18 14:08:21 +0600

    first commit

---
+++ Home.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- logo link-->
+    <link rel="icon"
+      href="../Group_Project_MAWE/piegon.ico">
+
+      <!--font awesome link-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+  <!--Custom CSS file-->
+  <link rel="stylesheet" href="style.css">
+    <title>Tourism Website</title>
+   <!--swiper css link--->
+
+    <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"
+/>
+</head>
+
+<body>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+   
+   <!--Header sections-starts---->
+
+   <section class="header">
+
+   <a href="Home.php" class ="logo">travel.</a>
+
+   <nav class ="navbar">
+    <a href="home.php">home</a>
+    <a href="about.php">about</a>
+    <a href="package.php">package</a>
+    <a href="book.php">book</a>
+   </nav>
+
+     <div id="menu-btn" class="fas fa-bars"></div>
+   </section>
+   
+   
+   <!--header sectiond ends--->
+   
+
+   <!-- home section starts-->
+
+<section class="home">
+   <div class="swiper home-slider">
+       <div class="swiper-wrapper">
+          <div class="swiper-slide slide" style="background:url(images/home-slide-1.jgp) no-repeat">
+              <div class="context">
+                   <span>explore, discover, travel</span>
+                   <h3>travel arround the world</h3>
+                    <a href="package.php" class="btn">discover  more</a>
+               </div>
+            </div>
+
+            <div class="swiper-slide slide" style="background:url(images/home-slide-2.jgp) no-repeat">
+              <div class="context">
+                   <span>explore, discover, travel</span>
+                   <h3>discover the new places</h3>
+                    <a href="package.php" class="btn">discover  more</a>
+               </div>
+            </div>
+
+            <div class="swiper-slide slide" style="background:url(images/home-slide-3.jgp) no-repeat">
+              <div class="context">
+                   <span>explore, discover, travel</span>
+                   <h3>make your tour worthwhile</h3>
+                    <a href="package.php" class="btn">discover  more</a>
+               </div>
+            </div>
+       </div>
+
+       <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+
+    </div>
+</section>
+
+<!-- home section ends -->
+   
+
+ <!-- service ection starts -->
+
+ 
+
+<section class="services">
+  <h1 class="heading-title">out services</h1>
+
+  <div class="box-container">
+    <div class="box">
+        <img src="../Group_Project_MAWE/adventure.jpg" alt="alternate">
+        <h3>adventure</h3>
+   </div>
+
+   <div class="box">
+        <img src="../Group_Project_MAWE/tourguide.jpg" alt="alternate">
+        <h3>tour guide</h3>
+   </div>
+
+   <div class="box">
+        <img src="../Group_Project_MAWE/treking.jpg" alt="alternate">
+        <h3>trekking</h3>
+   </div>
+
+   <div class="box">
+        <img src="../Group_Project_MAWE/camping.jpg" alt="alternate">
+        <h3>camp fire</h3>
+   </div>
+
+   <div class="box">
+        <img src="../Group_Project_MAWE/offroad.jpg" alt="alternate">
+        <h3>off road</h3>
+   </div>
+
+   <div class="box">
+        <img src="../Group_Project_MAWE/camping.jpg" alt="alternate">
+        <h3>camping</h3>
+   </div>
+
+</div>
+
+</section>
+
+ <!---service section ends here-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+   <!--footer section starts--->
+
+    <section class="footer">
+
+    <div class ="box-container">
+
+    <div class="box">
+     <h3>quick links</h3>
+    <a href="home.php"><i class="fas fa-angle-right"></i>home</a>
+    <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+    <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+    <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+
+    </div>
+    <div class="box">
+     <h3>extra links</h3>
+    <a href="#"><i class="fas fa-angle-right"></i>ask questions</a>
+    <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+    <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+    <a href="#"><i class="fas fa-angle-right"></i>terms of use</a>
+    
+    </div>
+
+    <div class="box">
+     <h3>contact info</h3>
+    <a href="#"><i class="fas fa-phone"></i>+088-01781-941604</a>
+  
+    <a href="#"><i class="fas fa-phone"></i>+088-01781-941604</a>
+    <a href="#"><i class="fas fa-phone"></i>+088-01640-941604</a>
+    <a href="#"><i class="fas fa-envelope"></i>ce20044@mbstu.ac.bd</a>
+    <a href="#"><i class="fas fa-map"></i>Dhaka, Bangladesh - 4001234</a>
+    
+    </div>
+
+    <div class="box">
+       
+       <h3>follow us</h3>
+       <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+       <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+       <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+       <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+
+       
+
+
+    </div>
+    </div>
+
+    <div class="credit">created by <span>Jalil: Sandija: Shamim:Megla</span>|all rights reserved!|</div>
+
+    </section>
+
+    <!--footer section ends-->
+    
+   
+    <!--swiper js link-->
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+
+    <script src="script.js"></script>
+    
+      
+
+
+</body>
+
+</html>
---
+++ book.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- logo link-->
+    <link rel="icon"
+      href="file:///F:/3.2%20all%20courses%20documents/Multimedia%20&%20Web%20Engineering/Group_Project_MAWE/piegon.ico">
+
+      <!--font awesome link-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+  <!--Custom CSS file-->
+  <link rel="stylesheet" href="style.css">
+    <title>about</title>
+   <!--swiper css link--->
+
+    <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"
+/>
+</head>
+
+<body>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+   
+   <!--Header sections-starts---->
+
+   <section class="header">
+
+   <a href="Home.php" class ="logo">travel.</a>
+
+   <nav class ="navbar">
+    <a href="home.php">home</a>
+    <a href="about.php">about</a>
+    <a href="package.php">package</a>
+    <a href="book.php">book</a>
+   </nav>
+
+     <div id="menu-btn" class="fas fa-bars"></div>
+   </section>
+   
+   
+   <!--header sectiond ends--->
+   
+
+    <div class="heading" style="background:url(file:///F:/3.2%20all%20courses%20documents/Multimedia%20&%20Web%20Engineering/Group_Project_MAWE/fontPicture.jpg) no-repeat"> <h1>book now</h1> </div>
+   
+   
+    
+
+<!-- booking section starts -->
+
+<section class = "booking">
+	<h1 class = "heading-title"> book your trip! </h1>
+	<form action = "book_form.php" method= "post" class = "book-form" >
+
+    <div class = "flex">
+    	<div class = "inputBox">
+    		<span> name: </span>
+    		<input type = "text" placeholder = "enter your name" name = "name">
+    	</div>
+    	<div class = "inputBox">
+    		<span> email: </span>
+    		<input type = "email" placeholder = "enter your email" name = "email">
+    	</div>
+    	<div class = "inputBox">
+    		<span> phone: </span>
+    		<input type = "number" placeholder = "enter your number" name = "phone">
+    	</div>
+    	<div class = "inputBox">
+    		<span> address: </span>
+    		<input type = "text" placeholder = "enter your address" name = "address">
+    	</div>
+    	<div class = "inputBox">
+    		<span> where to: </span>
+    		<input type = "text" placeholder = "place you want to visit" name = "location">
+    	</div>
+    	<div class = "inputBox">
+    		<span> how many: </span>
+    		<input type = "number" placeholder = "number of guests" name = "guests">
+    	</div>
+    	<div class = "inputBox">
+    		<span> arrivals: </span>
+    		<input type = "date"  name = "arrivals">
+    	</div>
+    	<div class = "inputBox">
+    		<span> leaving: </span>
+    		<input type = "date"  name = "leaving">
+    	</div>
+    </div>
+
+    <input type = "submit" value = "submit" class = "btn" name = "send">
+
+	</form>
+
+<!-- booking section end -->
+   
+   <!--footer section starts--->
+
+    <section class="footer">
+
+    <div class ="box-container">
+
+    <div class="box">
+     <h3>quick links</h3>
+    <a href="home.php"><i class="fas fa-angle-right"></i>home</a>
+    <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+    <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+    <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+
+    </div>
+    <div class="box">
+     <h3>extra links</h3>
+    <a href="#"><i class="fas fa-angle-right"></i>ask questions</a>
+    <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+    <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+    <a href="#"><i class="fas fa-angle-right"></i>terms of use</a>
+    
+    </div>
+
+    <div class="box">
+     <h3>contact info</h3>
+    <a href="#"><i class="fas fa-phone"></i>+088-01781-941604</a>
+  
+    <a href="#"><i class="fas fa-phone"></i>+088-01781-941604</a>
+    <a href="#"><i class="fas fa-phone"></i>+088-01640-941604</a>
+    <a href="#"><i class="fas fa-envelope"></i>ce20044@mbstu.ac.bd</a>
+    <a href="#"><i class="fas fa-map"></i>Dhaka, Bangladesh - 4001234</a>
+    
+    </div>
+
+    <div class="box">
+       
+       <h3>follow us</h3>
+       <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+       <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+       <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+       <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+
+       
+
+
+    </div>
+    </div>
+
+    <div class="credit">created by <span>Jalil: Sandija: Shamim:Megla</span>|all rights reserved!|</div>
+
+    </section>
+
+    <!--footer section ends-->
+    
+   
+    <!--swiper js link-->
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+
+    <script src="script.js"></script>
+    
+      
+
+
+</body>
+
+</html>
